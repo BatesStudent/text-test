641ec9f3610eab0a624a938cae5fa2712805a1c6
Adam <a.p.bates@hotmail.co.uk>
2017-11-06 14:19:59 +0000

    Changed JS

--- www/js/index.js
+++ www/js/index.js
@@ -11,27 +11,6 @@ function submitText() {
     alert(getItem('text'));
 }
 
-function storeValue(key, value) {
-	 /*var person= {
-		"firstName": "Homer",
-  		"lastName": "Simpson",
- 		"address": {
-    			"streetAddress": "742 Evergreen Terrace",
-    			"city": "Springfield",
-    			"state": "?"
-		}
-	}
-
-        storeItem("person", JSON.stringify(person));
-        storeItem("hair", "brown");
-        storeItem("beard", "messy");
-        storeItem("weather", "weird");
-        storeItem("date", "16th oct 2017");
-        
-        document.getElementById("storage1").innerHTML = "First name: " + JSON.parse(getItem("person")).firstName;
-        document.getElementById("storage2").innerHTML = "Storage items: " + window.localStorage.length;
-       */
-}
  function storeItem(key, item){
         window.localStorage.setItem( key, item );
     }
