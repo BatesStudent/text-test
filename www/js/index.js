0e60cc95bdb1c4ef2225e4afbff52c09cbd5c688
Adam <a.p.bates@hotmail.co.uk>
2017-11-06 14:16:31 +0000

    Stores texts

--- www/js/index.js
+++ www/js/index.js
@@ -7,9 +7,35 @@ $(document).on("pagecreate","#pageone",function(){
 
 function submitText() {
 	var text = $('#textinput').val();
-	alert(text);
+	storeItem('text',text);
+    alert(getItem('text'));
 }
 
 function storeValue(key, value) {
-	//add some code to store the key-value pair in persistant storage 
+	 /*var person= {
+		"firstName": "Homer",
+  		"lastName": "Simpson",
+ 		"address": {
+    			"streetAddress": "742 Evergreen Terrace",
+    			"city": "Springfield",
+    			"state": "?"
+		}
+	}
+
+        storeItem("person", JSON.stringify(person));
+        storeItem("hair", "brown");
+        storeItem("beard", "messy");
+        storeItem("weather", "weird");
+        storeItem("date", "16th oct 2017");
+        
+        document.getElementById("storage1").innerHTML = "First name: " + JSON.parse(getItem("person")).firstName;
+        document.getElementById("storage2").innerHTML = "Storage items: " + window.localStorage.length;
+       */
 }
+ function storeItem(key, item){
+        window.localStorage.setItem( key, item );
+    }
+
+    function getItem(key){
+        return window.localStorage.getItem( key );
+    }
